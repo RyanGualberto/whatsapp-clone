55ded478e576803d9d1f9fda24a17c54d3d436e6
Ryan Gualberto <RyanOliveiraGualberto@gmail.com>
2022-06-28 12:48:27 -0300

    aula 118: add: events menu attach

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -128,25 +128,71 @@ class WhatsAppController {
         });
 
         this.el.btnAttachPhoto.on('click', e => {
-            console.log('photo')
+            this.el.inputPhoto.click();
             // this.el.menuAttach.addClass('open');
         });
+
+        this.el.inputPhoto.on('change', e => {
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            });
+        });
+
         this.el.btnAttachCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open')
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            });
             console.log('camera')
-            // this.el.menuAttach.addClass('open');
         });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+            console.log('take picture');
+        })
+
         this.el.btnAttachDocument.on('click', e => {
-            console.log('Document')
-            // this.el.menuAttach.addClass('open');
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': '100%'
+            });
         });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnSendDocument.on('click', e => {
+            console.log('documento enviado');
+        });
+
         this.el.btnAttachContact.on('click', e => {
-            console.log('Contact')
-            // this.el.menuAttach.addClass('open');
+            this.el.modalContacts.show();
         });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        })
 
     }
 
-    CloserMenuAttach(e){
+    closeAllMainPanel() {
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+        this.el.modalContacts.hide();
+    }
+
+    CloserMenuAttach(e) {
         document.removeEventListener('click', this.CloserMenuAttach);
         this.el.menuAttach.removeClass('open');
     }
