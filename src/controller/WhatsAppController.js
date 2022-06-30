a8acdc42b835e980cbfcc3483088080467b0f81b
Ryan Gualberto <RyanOliveiraGualberto@gmail.com>
2022-06-30 08:20:16 -0300

    aula 121: add: messages input events

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -22,7 +22,7 @@ class WhatsAppController {
             this.style.display = 'block';
             return this;
         }
-        Element.prototype.toogle = function () {
+        Element.prototype.toggle = function () {
             this.style.display = (this.style.display === 'none') ? 'block' : 'none';
             return this;
         }
@@ -46,8 +46,8 @@ class WhatsAppController {
             this.classList.add(name);
             return this;
         }
-        Element.prototype.toogleClass = function (name) {
-            this.classList.toogle(name);
+        Element.prototype.toggleClass = function (name) {
+            this.classList.toggle(name);
             return this;
         }
         Element.prototype.hasClass = function (name) {
@@ -189,24 +189,56 @@ class WhatsAppController {
             this.startRecordMicrophoneTime();
         });
 
-        this.el.btnCancelMicrophone.on('click', e =>{
+        this.el.btnCancelMicrophone.on('click', e => {
             this.closeRecordMicrophone();
         });
 
-        this.el.btnFinishMicrophone.on('click', e =>{
+        this.el.btnFinishMicrophone.on('click', e => {
             this.closeRecordMicrophone();
         });
 
+        this.el.inputText.on('keypress', e => {
+            if (e.key=== 'Enter' && !e.ctrlKey){
+                e.preventDefault();
+                this.el.btnSend.click();
+            }
+        });
+
+        this.el.inputText.on('keyup', e => {
+            if (this.el.inputText.innerHTML.length) {
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            } else {
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+        });
+
+        this.el.btnSend.on('click', e => {
+            console.log(this.el.inputText.innerHTML)
+        });
+
+        this.el.btnEmojis.on('click', e => {
+            this.el.panelEmojis.toggleClass('open')
+        });
+
+        this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
+            emoji.on('click', e => {
+                console.log(emoji.dataset.unicode);
+            });
+        });
     }
 
-    startRecordMicrophoneTime(){
+    startRecordMicrophoneTime() {
         let start = Date.now();
         this._recordMicrophoneInterval = setInterval(() => {
             this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
         }, 100);
     }
 
-    closeRecordMicrophone(){
+    closeRecordMicrophone() {
         this.el.recordMicrophone.hide();
         this.el.btnSendMicrophone.show();
         clearInterval(this._recordMicrophoneInterval);
