305c9a20439cff6348d2a48647fe866ce752e7b8
Ryan Gualberto <ryanOliveiraGualberto@gmail.com>
2022-07-05 10:43:07 -0300

    Aula 147: reading messages in realtime

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -131,6 +131,9 @@ export class WhatsAppController {
     }
 
     setActiveChat(contact) {
+        if (this._contactActive) {
+            Message.getRef(this._contactActive.chatId).onSnapshot(() => { })
+        }
         this._contactActive = contact;
 
         this.el.activeName.innerHTML = contact.name;
@@ -146,6 +149,25 @@ export class WhatsAppController {
         this.el.main.css({
             display: 'flex'
         });
+
+        Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
+            .onSnapshot(docs => {
+                this.el.panelMessagesContainer.innerHTML = '';
+                docs.forEach(doc => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    
+                    if (!this.el.panelMessagesContainer.querySelector('#' + data.id)) {
+
+                        let message = new Message();
+                        message.fromJSON(data);                        
+                        let me = (data.from === this._user.email);
+                        let view = message.getViewElement(me);
+                        this.el.panelMessagesContainer.appendChild(view)
+                    }
+
+                });
+            })
     }
 
     loadElements() {
--- src/model/Message.js
+++ src/model/Message.js
@@ -1,4 +1,5 @@
 import { Firebase } from "../utils/Firebase";
+import { Format } from "../utils/Format";
 import { Model } from "./Model";
 
 export class Message extends Model {
@@ -6,6 +7,9 @@ export class Message extends Model {
         super();
     }
 
+
+    get id() { return this._data.id; }
+    set id(value) { return this._data.id = value; }
 
     get content() { return this._data.content; }
     set content(value) { return this._data.content = value; }
@@ -287,16 +291,16 @@ export class Message extends Model {
 
             default:
                 div.innerHTML = `
-                                            <div class="font-style _3DFk6 tail">
+                                            <div class="font-style _3DFk6 tail" id="${this.id}">
                                                 <span class="tail-container"></span>
                                                 <span class="tail-container highlight"></span>
                                                 <div class="Tkt2p">
                                                     <div class="_3zb-j ZhF0n">
-                                                        <span dir="ltr" class="selectable-text invisible-space message-text">Oi!</span>
+                                                        <span dir="ltr" class="selectable-text invisible-space message-text">${this.content}</span>
                                                     </div>
                                                     <div class="_2f-RV">
                                                         <div class="_1DZAH">
-                                                            <span class="msg-time">11:33</span>
+                                                            <span class="msg-time">${Format.timeMessage(this.timeStamp)}</span>
                                                         </div>
                                                     </div>
                                                 </div>
